96d91f758cf1693f277dcf349385bd940d27b4e4
AlanC8 <askvizi@bk.ru>
2025-12-01 20:58:34 +0500

    Add models for listings, properties, locations

--- settings/base.py
+++ settings/base.py
@@ -32,6 +32,8 @@ DJANGO_AND_THIRD_PARTY_APPS = [
 PROJECT_APPS = [
     "apps.abstracts",
     "apps.auths",
+    "apps.listings",
+    "apps.locations",
 ]
 INSTALLED_APPS = DJANGO_AND_THIRD_PARTY_APPS + PROJECT_APPS
 
