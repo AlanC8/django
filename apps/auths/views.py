fbc1bbfce29e9babe0369d9780b2130dec76b7b4
Mkhmt Core <mkhmtcore@gmail.com>
2025-12-01 22:41:08 +0500

    feat: added all auth to auth url

--- apps/auths/views.py
+++ apps/auths/views.py
@@ -9,7 +9,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from apps.auths.models import User
-from apps.auths.serializers import UserLoginSerializer
+from apps.auths.serializers import UserRegisterSerializer
 
 
 class UserViewSet(ViewSet):
@@ -18,58 +18,13 @@ class UserViewSet(ViewSet):
     """
 
     @action(
-        methods=("POST",),
-        detail=False,
-        url_path="login",
-        url_name="login",
-        permission_classes=(AllowAny,),
-    )
-    def login(
-        self, request: DRFRequest, *args: tuple[Any, ...], **kwargs: dict[str, Any]
-    ) -> DRFResponse:
-        """
-        Handle user login.
-
-        Parameters:
-            request: DRFRequest
-                The request object.
-            *args: tuple
-                Additional positional arguments.
-            **kwargs: dict
-                Additional keyword arguments.
-
-        Returns:
-            DRFResponse
-                Response containing user data or error message.
-        """
-
-        serializer: UserLoginSerializer = UserLoginSerializer(data=request.data)  # type: ignore
-
-        serializer.is_valid(raise_exception=True)
-
-        user: User = serializer.validated_data.pop("user")  # type: ignore
-
-        refresh: RefreshToken = RefreshToken.for_user(user)
-        access_token: str = str(refresh.access_token)
-
-        return DRFResponse(
-            data={
-                "id": user.id,  # type: ignore
-                "email": user.email,
-                "access": access_token,
-                "refresh": str(refresh),
-            },
-            status=HTTP_200_OK,
-        )
-
-    @action(
         methods=("GET",),
         detail=False,
-        url_name="personal_account",
-        url_path="personal_account",
+        url_name="me",
+        url_path="me",
         permission_classes=(IsAuthenticated,),
     )
-    def fetch_personal_account(
+    def get_user_info(
         self, request: DRFRequest, *args: tuple[Any, ...], **kwargs: dict[str, Any]
     ) -> DRFResponse:
         """
@@ -98,3 +53,49 @@ class UserViewSet(ViewSet):
             status=HTTP_200_OK,
         )
 
+    @action(
+        methods=("POST",),
+        detail=False,
+        url_name="register",
+        url_path="register",
+        permission_classes=(AllowAny,),
+    )
+    def register(
+        self, request: DRFRequest, *args: tuple[Any, ...], **kwargs: dict[str, Any]
+    ) -> DRFResponse:
+        """
+        Handle user registration.
+
+        Parameters:
+            request: DRFRequest
+                The request object.
+            *args: tuple
+                Additional positional arguments.
+            **kwargs: dict
+                Additional keyword arguments.
+
+        Returns:
+            DRFResponse
+                Response containing user data or error message.
+        """
+
+        serializer: UserRegisterSerializer = UserRegisterSerializer(data=request.data)  # type: ignore
+        serializer.is_valid(raise_exception=True)
+
+        user: User = User.objects.create_user(
+            email=serializer.validated_data["email"],  # type: ignore
+            password=serializer.validated_data["password"],  # type: ignore
+        )
+
+        refresh: RefreshToken = RefreshToken.for_user(user)
+        access_token: str = str(refresh.access_token)
+
+        return DRFResponse(
+            data={
+                "id": user.id,  # type: ignore
+                "email": user.email,
+                "access": access_token,
+                "refresh": str(refresh),
+            },
+            status=HTTP_200_OK,
+        )
